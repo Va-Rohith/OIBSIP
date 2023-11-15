db079e5c941282b16d5afa18e647d679db6a4dfd
Rohith <rohith6246@gmail.com>
2023-11-16 00:46:58 +0530

    Level-1

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,6 +1,8 @@
-function toggleMenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+    var navbar = document.getElementById("navbar");
+    if (navbar.style.display === "block") {
+        navbar.style.display = "none";
+    } else {
+        navbar.style.display = "block";
+    }
 }
